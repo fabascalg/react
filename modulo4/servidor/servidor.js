cb51419fa6a35de618d9477dcdfaad1bcbbac0a0
fabascalg <38880768+fabascalg@users.noreply.github.com>
2022-07-25 18:41:49 +0200

    primera hora de la tarde

--- modulo4/servidor/servidor.js
+++ modulo4/servidor/servidor.js
@@ -4,15 +4,24 @@ const port = 3000
 // activar CORS cross origin request sharing
 var cors = require('cors')
 app.use(cors())
+
 // creamos una lista de alumnos
 let alumnos=[];
 alumnos.push({"nombre":"pepe",nota:5},{"nombre":"ana",nota:7});
+
 // configuramos una url para devolver la lista de alumnos
 app.get("/alumnos", (req,res) => {
   res.send(alumnos)
 } )
-<<<<<<< HEAD
-app.delete("/Alumnos",(req,res)=>{
+
+// añadir un alumno
+app.post("/alumnos",(req,res)=>{
+  alumnos.push(req.body)
+  res.status(201).send()
+})
+
+// borrar por nombre
+app.delete("/alumnos/:nombre",(req,res)=>{
   let seleccionado = alumnos.filter(function(elemento){
     return elemento.nombre == req.params.nombre
   })[0];
@@ -20,22 +29,9 @@ app.delete("/Alumnos",(req,res)=>{
   alumnos.splice(indice,1)
   res.status(204).send()
 })
-=======
-// borrado
-app.delete("/alumnos/:nombre", (req,res){
-  // localizar <<nombre>>
-  let seleccionado= alumnos.filter(function(elemento){
-    return elemento.nombre == req.params.nombre;
-  })
-  // localizar el índice del encontrado
-  let indice = alumnos.indexOf(seleccionado);
-  alumnos.splice(indice,1);
-  res.status(204).send();
-})
-//
->>>>>>> c45d8b038dc1773c0a5a1c95a5a1b4f88e3fecae
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Servidor de Datos!!!')
 })
 
 app.listen(port, () => {
